6b9d4edb6d6b5d9b2e8b727fc708a7ecda27e49d
RAnderso0325 <reillyjanderson@gmail.com>
2017-11-26 21:28:33 -0800

    created game logic and reset button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var wins = 0, losses = 0;
+
 var cards = [
 {
 	rank: 'queen',
@@ -23,21 +25,50 @@ var cards = [
 
 var cardsInPlay = [];
 
-var checkforMatch = function () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	};
+var checkForMatch = function () {
+	if(cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		document.getElementById('wins').textContent = ++wins;
+		document.getElementById('matchInfo').textContent = "You found a math!";
+		} else {
+		document.getElementById('losses').textContent = ++losses;
+		document.getElementById('matchInfo').textContent = "Uhoh, those don't match. Try again!";
+		}
+	}
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	if(cardsInPlay.length === 2) {
+		document.getElementById('matchInfo').textContent = "Can you find a match?";
+		resetBoard();
+		return;
+	};
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	checkforMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 };
 
-flipCard(0);
-flipCard(3);
+var createBoard = function () {
+	for (var i=0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+var resetBoard = function () {
+	cardsInPlay = [];
+	var imgs = document.getElementById('game-board').getElementsByTagName('img');
+	for (i=0; i<cards.length; i++) {
+		imgs[i].setAttribute('src', 'images/back.png');
+	}
+};
+
+createBoard();
